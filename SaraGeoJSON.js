db71ce857c8593340dcb5af484f015e018f2a35c
Sara E <eichnersara@gmail.com>
2016-02-22 11:31:31 -0500

    finalized assignment #2
    problems worked out in js file. But I did not succeed in making a
    legend.

--- SaraGeoJSON.js
+++ SaraGeoJSON.js
@@ -1,5 +1,13 @@
-// This script demonstrates some simple things one can do with leaflet.js
+/* This is my homework from a couple of weeks ago, long overdue. 
+I used all these layers to see how they work together. In the future, I'd work to normalize population data in qgis or 
+in a data software like office libre. The json file I have for neighborhoods didn't have a clear option for 
+people per square mile so I just used the Pop number for this map. 
+The layering is confusing and it seems maps with too many layers with data bound to pop up covers can be 
+unwieldy. I'm interested in finding better ways to work with this.
 
+at the bottom of this file I tried to add a legend with no luck. I'll try again on the next assignment. 
+
+*/
 
 var map = L.map('map').setView([40.71,-73.93], 11);
 
@@ -23,6 +31,19 @@ var MapQuestAerialTiles = L.tileLayer('http://otile1.mqcdn.com/tiles/1.0.0/sat/{
   attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">'
 });
 
+/*
+adding basemaps:
+var OSMMapnikTiles = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+  attribution: 'Map Data and Tiles &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> Contributors'
+});
+
+....
+
+var baseMaps = {
+    "CartoDB": CartoDBTiles,
+    "OSM Mapnik": OSMMapnikTiles
+};
+*/
 
 // create global variables we can use for layer controls
 var subwayLinesGeoJSON;
@@ -42,8 +63,8 @@ function addSubwayLines() {
         // style for subway lines
         var subwayStyle = {
             "color": "#8c8c8c",
-            "weight": 2,
-            "opacity": 0.80
+            "weight": 3,
+            "opacity": 0.50
         };
 
         // function that binds popup data to subway lines
@@ -97,7 +118,8 @@ function addfloodZones() {
                 fillColor = "#d0d1e6";
             }
              if(value == 7){ 
-                fillColor = "#000000";
+                fillColor = "#003366";
+    
             }
              if(value === "X"){ 
                 fillColor = "#000",
@@ -174,31 +196,32 @@ $.getJSON( "geojson/NYC_neighborhood_data.geojson", function( data ) {
 
         var value = feature.properties.Pop;
         var fillColor = null;
-        if(value >= 0 && value <=0.1){
-            fillColor = "#fee5d9";
+        var fillOpacity = .5;
+        if(value >= 0 && value <=50000){
+            fillColor = "#f7f7f7";
         }
-        if(value >0.1 && value <=0.15){
-            fillColor = "#fcbba1";
+        if(value > 50001 && value <=100000){
+            fillColor = "#cccccc";
         }
-        if(value >0.15 && value<=0.2){
-            fillColor = "#fc9272";
+        if(value > 100001 && value<=150000){
+            fillColor = "#969696";
         }
-        if(value > 0.2 && value <=0.3){
-            fillColor = "#fb6a4a";
+        if(value > 150001 && value <=200000){
+            fillColor = "#636363";
         }
-        if(value > 0.3 && value <=0.4) { 
-            fillColor = "#de2d26";
+        if(value > 200001 && value <=300000) { 
+            fillColor = "#252525";
         }
-        if(value > 0.4) { 
-            fillColor = "#a50f15";
-        }
+
+
+        
         
 // this var style = is for all the other styles of each neighborhood
         var style = {
             weight: 1,
-            opacity: .1,
-            color: 'white',
-            fillOpacity: 0.2,
+            opacity: 0.3,
+            color: "#454545",
+            fillOpacity: fillOpacity,
             fillColor: fillColor
         };
 // return --this tells computer to display style
@@ -207,7 +230,7 @@ $.getJSON( "geojson/NYC_neighborhood_data.geojson", function( data ) {
 
     var populationClick = function (feature, layer) {
         // let's bind some feature properties to a pop up
-        layer.bindPopup("<strong>Neighborhood:</strong> " + feature.properties.NYC_NEIG + "<br /><strong>Population</strong>" + feature.properties.Pop);
+        layer.bindPopup("<strong>Neighborhood: </strong> " + feature.properties.NYC_NEIG + "<br /><strong>Population: </strong>" + feature.properties.Pop);
     }
 //This is a loop below: We've used one for each layer
     neighborhoodDataGeoJSON = L.geoJson(neighborhoodData, {
@@ -216,11 +239,12 @@ $.getJSON( "geojson/NYC_neighborhood_data.geojson", function( data ) {
     });
 
      // now lets add the data to the map in the order that we want it to appear
+ 
+       // neighborhoods: 
+        neighborhoodDataGeoJSON.addTo(map);
+
         // flood zones first
         floodZonesGeoJSON.addTo(map);
-
-        // neighborhoods: leave this off for now
-       neighborhoodDataGeoJSON.addTo(map);
 
         // subway lines next
         subwayLinesGeoJSON.addTo(map);
@@ -254,8 +278,37 @@ function createLayerControls(){
 
     // add control
     L.control.layers(baseMaps, overlayMaps).addTo(map);
-
 }
+
+/* I'd like to create a legend, but I don't understand what the variables other than div are (Grades, Amounts or Labels) refer to
+where are they defined? How do they relate back to the population value? 
+- How does this legend get tied to the neighborhood map layer rather than flood zones? 
+
+This is getting close below, but I dont' know how to define colors? If I understand the 
+initial variables better, maybe I cna set tie the legend back to the fill colors in 
+neighborhood layer better. should it be Pop? Neighborhoods
+
+var legend = L.control({position: 'bottomright'});
+legend.onAdd = function (map) { 
+    var div = L.DomUtil.create('div', 'legend'),
+        amounts = [0, 50000, 100000, 150000, 200000, 2500000],
+        labels = [];
+
+        // for (var i = 0; <amounts.length; i ++) {
+        //     div.innerHTML += '<p><strong>Amounts</strong></p>';
+        for (var i = 0; i <= amounts.length; i++) {
+        div.innerHTML += '<i style="background:' + fillColor(amounts[i] + 1) + '"></i> ' + amounts[i] + (amounts[i + 1] ? '&ndash;' + amounts[i + 1] + '<br />' : '+ <br />');
+        }
+
+
+    return div;
+};
+
+legend.addTo(map);
+        
+
+*/ 
+//        
 
 
 
